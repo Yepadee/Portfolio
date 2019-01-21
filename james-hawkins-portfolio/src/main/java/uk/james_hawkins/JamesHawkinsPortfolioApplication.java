637bca5208e8771202731d976b4c925c87db04f9
James <james@hawkinsplace.co.uk>
2019-01-21 15:56:18 +0000

    initial-commit

---
+++ james-hawkins-portfolio/src/main/java/uk/james_hawkins/JamesHawkinsPortfolioApplication.java
@@ -0,0 +1,14 @@
+package uk.james_hawkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamesHawkinsPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamesHawkinsPortfolioApplication.class, args);
+	}
+
+}
+
---
+++ james-hawkins-portfolio/src/test/java/uk/james_hawkins/JamesHawkinsPortfolioApplicationTests.java
@@ -0,0 +1,17 @@
+package uk.james_hawkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class JamesHawkinsPortfolioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
