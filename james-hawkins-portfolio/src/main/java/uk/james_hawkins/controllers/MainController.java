08543967c00780e64bcc31386cdec175961371cc
James <james@hawkinsplace.co.uk>
2019-01-22 14:07:06 +0000

    Made oauth key private

--- james-hawkins-portfolio/src/main/java/uk/james_hawkins/controllers/MainController.java
+++ james-hawkins-portfolio/src/main/java/uk/james_hawkins/controllers/MainController.java
@@ -3,26 +3,28 @@ package uk.james_hawkins.controllers;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
-
 import org.eclipse.egit.github.core.Repository;
-import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:otherprops.properties")
 @Controller
 public class MainController {
 
+	
+    @Value("${oauth}")
+    private String oauth;
+    
 	Logger logger = LoggerFactory.getLogger(MainController.class);
-
 	
 	@GetMapping("/")
 	private String getMainPage(Model model) {
-		GitHubClient client = new GitHubClient();
-		client.setOAuth2Token("44ea40ec83ef39e053378d00c91b8240006c4e8c");
 		RepositoryService service = new RepositoryService();
 		try {
 			List<Repository> repos = service.getRepositories("yepadee");
